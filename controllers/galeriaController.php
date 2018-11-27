a0b7b7cb67a6d8cd57c57bbe10a06862644146a5
Lucas Passos <lucasplpx@gmail.com>
2018-11-27 09:22:24 -0200

    autoload+core

---
+++ controllers/galeriaController.php
@@ -0,0 +1,13 @@
+<?php
+class galeriaController {
+
+
+    public function index(){
+        echo 'Olha a galeria !';
+    }
+
+    public function abrir($id){
+        echo "Abrindo a galeria ".$id;
+    }
+
+}
---
+++ controllers/homeController.php
@@ -0,0 +1,14 @@
+<?php
+class homeController {
+
+    public function index(){
+
+        echo 'Olá Mundo';
+    }
+
+    public function teste(){
+        echo "Este é um teste";
+    }
+
+
+}
